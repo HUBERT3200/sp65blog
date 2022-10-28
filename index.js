5d9de45cb137b04ca1c01288088613db5c3534a8
HUBERT3200 <street.cartoons.art@gmail.com>
2022-10-28 07:52:41 +0200

    h

--- index.js
+++ index.js
@@ -9,5 +9,21 @@ form.addEventListener('submit', (e) => {
     if (title.value === '' || title.value == null) {
         messages.push('Musisz nadać tytuł')
     }
+    
+    if (title.value.length >= 20) {
+        messages.push('Nadaj krutszy tytuł')
+    }
+    
+    
+    
+    
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorDiv.innerText = messages.join(', ')
+    }
     e.preventDefault()
+    setTimeout(function(){
+        console.log("Wyczyszczono errorDiv")
+        errorDiv.innerText = ' '
+    }, 3000);
 })
